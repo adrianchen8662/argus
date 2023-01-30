3841522c4f56f32f66a737af1d9851211f72af50
Adrian Chen <adrianchen8662@gmail.com>
2023-01-30 16:10:35 -0500

    Update to relative path and more notes

--- doorbell/src/main.py
+++ doorbell/src/main.py
@@ -1,10 +1,9 @@
 import cv2
 import sys
-import socket
+import socket # maybe use this for sending files, but if there's a library that does HTTPS connections use that instead
+from pathlib import Path
 
-# TODO: convert to relative pathing
-cascPath = r"C:\Users\adria\OneDrive\Documents\argus\doorbell\data\haarcascade_frontalface_default.xml"
-# cascPath = r"C:\Users\adria\OneDrive\Documents\argus\doorbell\data\haarcascade_profileface.xml"
+cascPath = str(Path(__file__).parent.parent) + r"\data\haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
@@ -18,7 +17,8 @@ while True:
     # detect a face
     # NOTE: Not very robust in low-light conditions and if not facing camera
     # Fix is either more data of other conditions or find a different library
-    # Compreface has facial detection, but is a separate program that runs parallel
+    # Compreface has facial detection, but is a separate program that runs parallel in a docker
+    # detectMultiscale was bounding box values, but no weight output
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -26,6 +26,7 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE, 
     )
+    # detectMultiscale3 has weight output, but no x y w h values for creating bounding box
     faceConfidence = faceCascade.detectMultiScale3(
         gray,
         scaleFactor=1.1,
@@ -35,6 +36,7 @@ while True:
         outputRejectLevels = True
     )
     weights = str(faceConfidence[2])
+    # detectMultiscale outputs a list of rectangles. This takes the first rectangle and gets the weight, if an entry exists
     try:
         (faceConfidence[2][0])
     except:
@@ -46,6 +48,7 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     # TODO: send the image file if the face fits a certain size and confidence values
+    # possibly send two different images. One with just the face, and one the entire frame with or without the bounding box
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
