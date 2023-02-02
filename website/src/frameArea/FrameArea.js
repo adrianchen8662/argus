11fa8b9787567d905beb724acadd07053bebc136
Ainesh Sootha <aineshsootha@gmail.com>
2023-02-02 18:55:26 -0500

    Basic Frame Frontend

---
+++ website/src/frameArea/FrameArea.js
@@ -0,0 +1,22 @@
+import './FrameArea.css';
+import React from 'react';
+import Frame from './frame/Frame';
+import Timeline from './timeline/Timeline';
+
+class FrameArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="frameArea">
+        <Frame />
+        <Timeline />
+      </div>
+    );
+  }
+}
+
+export default FrameArea;
