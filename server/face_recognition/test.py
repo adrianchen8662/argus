8d81bf5000051f9e2f248e3b01c974615c568d30
Adrian Chen <33459725+adrianchen8662@users.noreply.github.com>
2023-03-01 22:34:48 -0500

    Update test.py

--- server/face_recognition/test.py
+++ server/face_recognition/test.py
@@ -5,9 +5,9 @@ from compreface.collections.face_collections import Subjects
 
 # https://github.com/exadel-inc/compreface-python-sdk
 
-DOMAIN: str = 'http://100.106.18.99'
-PORT: str = '8000'
-API_KEY: str = '00000000-0000-0000-0000-000000000002'
+DOMAIN: str = "http://100.106.18.99"
+PORT: str = "8000"
+API_KEY: str = "00000000-0000-0000-0000-000000000002"
 
 compre_face: CompreFace = CompreFace(DOMAIN, PORT)
 
