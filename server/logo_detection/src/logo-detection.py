5ccfefcbe97b0beda1c6243b30bbbf94dd95459f
Justin Chan <justinchanchan8@gmail.com>
2023-03-06 15:23:42 -0500

    logo-detect with fixed formatting

--- server/logo_detection/src/logo-detection.py
+++ server/logo_detection/src/logo-detection.py
@@ -10,7 +10,7 @@ execution_path = os.getcwd()
 
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolov3.pt"))
+detector.setModelPath(os.path.join(execution_path , "yolov3.pt"))
 detector.loadModel()
 
 folder_dir = ROOT_DIR + "/images/cows_input/front/"
@@ -18,13 +18,12 @@ output_folder_dir = ROOT_DIR + "/images/cows_output/"
 
 confidence = []
 for images in os.listdir(folder_dir):
-    detections = detector.detectObjectsFromImage(input_image=os.path.join(folder_dir , images)) #, output_image_path=os.path.join(output_folder_dir, images))
+    detections = detector.detectObjectsFromImage(
+        input_image=os.path.join(folder_dir, images)
+    )  # , output_image_path=os.path.join(output_folder_dir, images))
     if len(detections) > 0:
         for obj in detections:
-            if obj["name"] == "cow":
-                # print("Cow at the Door:")    
+            if obj["name"] == "cow":  
                 img = cv2.imread(folder_dir + images)
                 cv2.imwrite(output_folder_dir + images, img)
             # print(obj["name"] , " : " , obj["percentage_probability"] )
-
-
