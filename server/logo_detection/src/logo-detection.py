04a2c91d3ddcb29108b4236f4f7f95be9b8c9a76
Justin Chan <justinchanchan8@gmail.com>
2023-03-06 15:25:57 -0500

    logo-detect fixed formatting

--- server/logo_detection/src/logo-detection.py
+++ server/logo_detection/src/logo-detection.py
@@ -5,12 +5,11 @@ import cv2
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path , "yolov3.pt"))
+detector.setModelPath(os.path.join(execution_path, "yolov3.pt"))
 detector.loadModel()
 
 folder_dir = ROOT_DIR + "/images/cows_input/front/"
@@ -23,7 +22,7 @@ for images in os.listdir(folder_dir):
     )  # , output_image_path=os.path.join(output_folder_dir, images))
     if len(detections) > 0:
         for obj in detections:
-            if obj["name"] == "cow":  
+            if obj["name"] == "cow":
                 img = cv2.imread(folder_dir + images)
                 cv2.imwrite(output_folder_dir + images, img)
             # print(obj["name"] , " : " , obj["percentage_probability"] )
