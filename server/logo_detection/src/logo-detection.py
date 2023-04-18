726acec067e5a35650063375b9e614ecc0a46ba8
Justin Chan <justinchanchan8@gmail.com>
2023-04-18 11:01:12 -0400

    updated black formatting

--- server/logo_detection/src/logo-detection.py
+++ server/logo_detection/src/logo-detection.py
@@ -6,9 +6,6 @@ from PIL import Image
 
 
 def logo_detection(input_image_path, curr_path):
-    
-
-
     detector = ObjectDetection()
     print(detector)
     detector.setModelTypeAsYOLOv3()
@@ -23,21 +20,24 @@ def logo_detection(input_image_path, curr_path):
     confidence = []
     for images in os.listdir(folder_dir):
         input_image_path = os.path.join(folder_dir, images)
-        # output_image_path=os.path.join(output_folder_dir, images)
-        # detections = detector.detectCustomObjectsFromImage( custom_objects=custom, input_image=os.path.join(execution_path , "image3.jpg"), output_image_path=os.path.join(execution_path , "image3new-custom.jpg"), minimum_percentage_probability=30)
+
         custom_person = detector.CustomObjects(person=True)
-        detections = detector.detectObjectsFromImage(custom_objects=custom_person, input_image=input_image_path)#,extract_detected_objects=True)
+        detections = detector.detectObjectsFromImage(
+            custom_objects=custom_person, input_image=input_image_path
+        )  # ,extract_detected_objects=True)
         print(detections)
-        
-        x1,y1,x2,y2 = detections[-1]["box_points"]
+
+        x1, y1, x2, y2 = detections[-1]["box_points"]
         img = Image.open(os.path.join(folder_dir, images))
         np_img = np.asarray(img)
-        check = Image.fromarray(np_img[y1:, x1:x2], 'RGB')
+        check = Image.fromarray(np_img[y1:, x1:x2], "RGB")
         # check.show()
 
         custom_bird = detector.CustomObjects(stop_sign=True)
-        detections = detector.detectObjectsFromImage(custom_objects=custom_bird,input_image=np_img[y1:, x1:x2])
-        # detections = detector.detectCustomObjectsFromImage(custom_objects=custom, input_image=os.path.join(folder_dir, images), output_image_path=os.path.join(output_folder_dir, images), minimum_percentage_probability=30)
+        detections = detector.detectObjectsFromImage(
+            custom_objects=custom_bird, input_image=np_img[y1:, x1:x2]
+        )
+
         print(detections)
         if len(detections) != 0:
             return True
@@ -56,5 +56,5 @@ if __name__ == "__main__":
 
     folder_dir = ROOT_DIR + "/images/cows_input/front/"
     output_folder_dir = ROOT_DIR + "/images/cows_output/"
-    
+
     detect = logo_detection(folder_dir, execution_path)
