ebadf560745fd17de1ae222bacbab8dffad6f205
Justin Chan <justinchanchan8@gmail.com>
2023-04-17 20:39:23 -0400

    update on logo detection for detecting people + stop sign

--- server/logo_detection/src/run.sh
+++ server/logo_detection/src/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rm /Users/justinchan/Desktop/devrep/argus/server/images/cows_output/*
+rm /Users/justinchan/Desktop/devrep/argus/server/logo_detection/images/cows_output/*
 python logo-detection.py
+ 
